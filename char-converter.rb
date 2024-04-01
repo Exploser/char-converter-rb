17c4179048c428131fd73291f92ad59f7be919d3
Exploser <exploserbusiness@gmail.com>
2024-04-01 11:43:26 -0400

    Implement a hashmap to get each character

--- char-converter.rb
+++ char-converter.rb
@@ -1,12 +1,17 @@
-
 # char-converter.rb
 
-# Check if any arguments were passed
-if ARGV.length == 0
-  puts "No arguments were provided."
-else
-  puts "Provided Arguments:"
-  ARGV.each_with_index do |arg, index|
-    puts "Argument #{index + 1}: #{arg}"
-  end
+# Get the string from the user
+input_string = ARGV[0] || "Test"
+
+# Empty hash to store char counts
+char_counts = Hash.new(0)
+
+input_string.each_char do |char|
+  char_counts[char] += 1
+end
+
+# Print the char
+puts "Character counts:"
+char_counts.each do |char, count|
+  puts "#{char}: #{count}"
 end
