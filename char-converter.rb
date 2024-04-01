9b7f513f2dd873da2d8ff8d290a6bc475f9dd294
Exploser <exploserbusiness@gmail.com>
2024-04-01 12:06:01 -0400

    Add a function to convert string to ASCII value Not using a hash for this implementation.

--- char-converter.rb
+++ char-converter.rb
@@ -1,17 +1,13 @@
 # char-converter.rb
 
+def to_ascii(string)
+  string.each_char.map{ |char| char.ord }
+end
+
 # Get the string from the user
 input_string = ARGV[0] || "Test"
 
-# Empty hash to store char counts
-char_counts = Hash.new(0)
+puts "Original String: #{input_string}"
 
-input_string.each_char do |char|
-  char_counts[char] += 1
-end
+puts "ASCII Values: #{to_ascii(input_string).join(', ')}"
 
-# Print the char
-puts "Character counts:"
-char_counts.each do |char, count|
-  puts "#{char}: #{count}"
-end
