acd5c1c1e8b2ea92f0c60778fb47cc198f7b31f6
Exploser <exploserbusiness@gmail.com>
2024-04-10 00:29:33 -0400

    Rewrote the tool to perform the ideal functionalities

--- char-converter.rb
+++ char-converter.rb
@@ -1,13 +1,43 @@
-# char-converter.rb
+#!/usr/bin/env ruby
 
-def to_ascii(string)
-  string.each_char.map{ |char| char.ord }
+# Check if the correct number of arguments are provided
+if ARGV.length != 2
+  puts "Usage: #{__FILE__} <flag> <string>"
+  puts "Flags:"
+  puts "  -b  Convert string to binary"
+  puts "  -h  Convert string to hexadecimal"
+  puts "  -a  Convert string to ASCII (decimal values)"
+  puts "  -o  Convert string to octal"
+  puts "  -n  Convert UTF-8 binary to char"
+  exit
 end
 
-# Get the string from the user
-input_string = ARGV[0] || "Test"
+flag, input_string = ARGV
 
-puts "Original String: #{input_string}"
+puts "Input: #{input_string}"
 
-puts "ASCII Values: #{to_ascii(input_string).join(', ')}"
+case flag
+
+when '-b'
+  # Convert string to binary
+  puts "Binary: "+input_string.bytes.map { |b| b.to_s(2).rjust(8, '0') }.join(' ')
+when '-h'
+  # Convert string to hexadecimal
+  puts "HEX: "+input_string.bytes.map { |b| b.to_s(16).rjust(2, '0') }.join(' ')
+when '-a'
+  # Convert string to ASCII (decimal values)
+  puts "ASCII: "+input_string.bytes.join(' ')
+when '-o'
+  # Convert string to octal
+  puts "Octal: "+input_string.bytes.map { |b| b.to_s(8).rjust(3, '0') }.join(' ')
+when '-n'
+  if (((input_string.length ) % 8 ) == 0 )
+    # Convert UTF-8 binary to char
+    puts "UTF-8: "+input_string.split(' ').map { |b| b.to_i(2).chr(Encoding::UTF_8) }.join
+  else
+    puts "Invalid Binary"
+  end
+else
+  puts "Invalid flag: #{flag}"
+end
 
